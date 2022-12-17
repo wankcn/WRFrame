5af2fa3b66ba83b42e66c73adbb111f5df42e169
wenruo <wankcn@icloud.com>
2022-12-17 16:14:44 +0800

    特性扩展方法

--- Assets/Frameworks/Base/Extension/Extension.cs
+++ Assets/Frameworks/Base/Extension/Extension.cs
@@ -1,10 +1,27 @@
+using System;
+using System.Reflection;
 using UnityEngine;
 
 namespace Frameworks
 {
     public static class Extension
     {
-        
-        
+        /// <summary>
+        /// 获取特性
+        /// </summary>
+        public static T GetAttribute<T>(this object obj) where T : Attribute
+        {
+            return obj.GetType().GetCustomAttribute<T>();
+        }
+
+        /// <summary>
+        /// 获取特性
+        /// </summary>
+        /// <param name="obj"></param>
+        /// <param name="type">特性所在的类型</param>
+        public static T GetAttribute<T>(this object obj, Type type) where T : Attribute
+        {
+            return type.GetCustomAttribute<T>();
+        }
     }
 }
--- Assets/Test/Test.cs
+++ Assets/Test/Test.cs
@@ -1,7 +1,20 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Test : MonoBehaviour
 {
+}
+
+
+[AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+public class TextAttribute : Attribute
+{
+    public bool canSwitch;
+
+    public TextAttribute(bool canSwitch)
+    {
+        this.canSwitch = canSwitch;
+    }
 }
